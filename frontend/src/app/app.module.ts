320c08653e2560e69d52f77c67dbfa0a4506257b
Asma2212 <bey.asma22@gmail.com>
2025-04-06 20:02:37 +0200

    cleaning project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,44 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTrashComponent } from './add-trash/add-trash.component';
-import { CommentsComponent } from './comments/comments.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './utils/sidebar/sidebar.component';
+import { ChartDataComponent } from './utils/chart/chart.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { Chart2Component } from './utils/chart2/chart2.component';
+import { TrashComponent } from './add-trash/add-trash.component';
+import { CommentsComponent } from './comments/comments.component';
+import { RapportComponent } from './rapport/rapport.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
-import { RapportComponent } from './rapport/rapport.component';
 import { ReceiversComponent } from './receivers/receivers.component';
-import { RegisterComponent } from './register/register.component';
 import { TrashListComponent } from './trash-list/trash-list.component';
-import { ChartComponent } from './utils/chart/chart.component';
-import { Chart2Component } from './utils/chart2/chart2.component';
-import { SidebarComponent } from './utils/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTrashComponent,
-    CommentsComponent,
-    DashboardComponent,
     LandingPageComponent,
     LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    SidebarComponent,
+    ChartDataComponent,
+    Chart2Component,
+    TrashComponent,
+    CommentsComponent,
+    RapportComponent,
     LogoutComponent,
     ProfileComponent,
-    RapportComponent,
     ReceiversComponent,
-    RegisterComponent,
     TrashListComponent,
-    ChartComponent,
-    Chart2Component,
-    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgApexchartsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
