49fe551435af83c6ea26b24b63bb2d728911a897
meryemidboucair <125923892+meryemidboucair@users.noreply.github.com>
2025-04-06 17:45:09 +0100

    Update dashboard.component.ts

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  user : any;
+  trashs : any[] = [];
+
+
+  constructor(private http:HttpClient) {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/api/trash/all').subscribe({
+      next: (data) => {
+        this.trashs = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
 
 }
