d6ab8c6b0edac9ec282682e609e3cb7b82007634
Jihen BOUKHADHRA <129115407+Jihen-Boukhadhra@users.noreply.github.com>
2025-04-06 17:55:58 +0100

    Update rapport.component.ts

--- frontend/src/app/rapport/rapport.component.ts
+++ frontend/src/app/rapport/rapport.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./rapport.component.css']
 })
 export class RapportComponent {
+  title = "";
+  content = "";
+  rapports : any[] = [];
+
+  user: any;
+
+
+  ngOnInit() {
+    this.getRapports();
+  }
+
+  constructor(private http: HttpClient) {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  getRapports() {
+    this.http.get('http://localhost:3000/api/rapport/all').subscribe(
+      {
+        next: (data: any) => {
+          this.rapports = data;
+        },
+        error: (error: any) => {
+          console.log(error);
+        }
+      }
+    )
+  }
+
+  addRapport() {
+    const data = {
+      title: this.title,
+      author: this.user.name,
+      content: this.content
+    }
+
+    this.http.post('http://localhost:3000/api/rapport/create', data).subscribe(
+      {
+        next: (data: any) => {
+          this.title = "";
+          this.content = "";
+          this.getRapports();
+        },
+        error: (error: any) => {
+          console.log(error);
+        }
+      }
+    )
+
+
+
+  }
 
 }
