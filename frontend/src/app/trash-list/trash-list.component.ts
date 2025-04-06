4afb0fcf72bdf5899d7aafc4841c55a50a54f148
Jihen BOUKHADHRA <129115407+Jihen-Boukhadhra@users.noreply.github.com>
2025-04-06 17:44:27 +0100

    Update trash-list.component.ts

--- frontend/src/app/trash-list/trash-list.component.ts
+++ frontend/src/app/trash-list/trash-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./trash-list.component.css']
 })
 export class TrashListComponent {
+  user : any;
+  trashs : any[] = [];
+
+
+  constructor(private http:HttpClient) {
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  getAllTrash(){
+    this.http.get<any>('http://localhost:3000/api/trash/all').subscribe({
+      next: (data) => {
+        this.trashs = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+
+  ngOnInit(): void {
+    this.getAllTrash();
+    
+  }
+
+  selectTrash(id : any){
+    //update trash
+    this.http.put<any>(`http://localhost:3000/api/trash/update/${id}`, {
+      receiver: this.user._id
+    }).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.getAllTrash();
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
 
 }
