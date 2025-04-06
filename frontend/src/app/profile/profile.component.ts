0333f2a625d5b445690da48af1064c68e52455ba
meryemidboucair <125923892+meryemidboucair@users.noreply.github.com>
2025-04-06 17:57:26 +0100

    Update profile.component.ts

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  user : any;
+
+  name = "";
+  newpassword = "";
+  confirmnewpassword = "";
+
+
+
+  constructor(private http:HttpClient) {}
+
+
+  ngOnInit(): void {
+
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
+
+    this.name = this.user.name;
+
+
+
+    this.http.get(`http://localhost:3000/api/auth/${this.user._id}`).subscribe({
+      next: (data) => {
+        this.user = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+
+  updateProfile(){
+    if (this.newpassword !== this.confirmnewpassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+
+
+    const data = {
+      name: this.name,
+      password: this.newpassword,
+      role: this.user.role
+    }
+
+    this.http.put(`http://localhost:3000/api/auth/${this.user._id}`, data).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.user = data;
+        localStorage.setItem('user', JSON.stringify(this.user));
+        window.location.href = '/profile';
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+
 
 }
