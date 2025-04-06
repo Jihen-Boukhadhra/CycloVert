86ea3d3f4dd6293a4477a70570b56a74c0319119
Jihen BOUKHADHRA <129115407+Jihen-Boukhadhra@users.noreply.github.com>
2025-04-06 17:50:42 +0100

    Update register.component.ts

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  fullName = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  accountType = '';
+
+  message = '';
+
+  constructor(private http : HttpClient) { }
+
+
+
+  register(){
+    const data = {
+      name : this.fullName,
+      email : this.email,
+      password : this.password,
+      type : this.accountType
+    }
+
+    if (this.password !== this.confirmPassword){
+      this.message = 'Passwords do not match';
+      return;
+    }
+
+    this.http.post('http://localhost:3000/api/auth/register', data).subscribe(
+      {
+        next: (data: any) => {
+          console.log(data);
+          window.location.href = '/login';
+        },
+        error: (error: any) => {
+          this.message = error.error.message;
+          console.log(error);
+        }
+      }
+    )
+
+    
+    
+  }
 
 }
