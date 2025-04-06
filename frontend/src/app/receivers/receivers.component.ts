a130685399c6af640a65152f3763788dd34d0565
Jihen BOUKHADHRA <129115407+Jihen-Boukhadhra@users.noreply.github.com>
2025-04-06 17:53:11 +0100

    Update receivers.component.ts

--- frontend/src/app/receivers/receivers.component.ts
+++ frontend/src/app/receivers/receivers.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./receivers.component.css']
 })
 export class ReceiversComponent {
+  receivers : any[] = [];
+
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/api/auth/all').subscribe({
+      next: (data) => {
+        data.filter((user: any) => {
+          if (user.type === 'Receiver') {
+            this.receivers.push(user);
+          }
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 
 }
